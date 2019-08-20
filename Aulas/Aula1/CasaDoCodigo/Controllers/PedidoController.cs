543bc6f2f1b44da9fde03f1de2f1da176cc334a7
Fernando Salgado <fernando_salgado243@hotmail.com>
2019-08-20 15:45:12 -0300

    Capitulo 4

---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+
+        public PedidoController(IProdutoRepository produtoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View(produtoRepository.GetProdutos());
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+    }
+}
